8dc0900d761b791f4a057b77ea4047366353e4fe
IntelliSenseIoT <intellisenseltd@outlook.com>
2020-05-13 20:48:52 +0200

    Add IoT Reference

--- XServerIoTOnboardTaskProject/XServerIoTOnboardTaskProject/StartupTask.cs
+++ XServerIoTOnboardTaskProject/XServerIoTOnboardTaskProject/StartupTask.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
 
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
 namespace XServerIoTOnboardTaskProject
 {
@@ -13,13 +12,7 @@ namespace XServerIoTOnboardTaskProject
     {
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
+          
         }
     }
 }
